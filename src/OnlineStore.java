b7948e618a0e3c54aa9112472d95dc5c8d3f35f3
sali72 <sahashemi072@gmail.com>
2019-04-06 16:43:00 +0430

    First step: creating first screen of user interface

---
+++ src/OnlineStore.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class OnlineStore {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("**********Ali's ONLINE STORE **********" +
+                    "\n" + "1) customer" +
+                    "\n" + "2) client" +
+                    "\n" + "3) exit app");
+        int x = input.nextInt();
+        switch (x) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+                default:
+                    System.out.println("please choose a valid option");
+        }
+
+    }
+}
