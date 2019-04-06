872e0b8556ec286f77f7d2db3d5cf419e83ee40e
sali72 <sahashemi072@gmail.com>
2019-04-06 18:59:42 +0430

    sign up method added

--- src/Customer.java
+++ src/Customer.java
@@ -9,7 +9,55 @@ public class Customer {
     private String email;
     private String address;
     private int postal_code;
-
-
-
+    ////////////////////////////// getters and setters///////////////////////////////////
+    //password
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getPassword() {
+        return password;
+    }
+    //name
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    //last name
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+    public String getLast_name() {
+        return last_name;
+    }
+    //phone number
+    public void setPhone_number(int phone_number) {
+        this.phone_number = phone_number;
+    }
+    public int getPhone_number() {
+        return phone_number;
+    }
+    //email
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getEmail() {
+        return email;
+    }
+    //address
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getAddress() {
+        return address;
+    }
+    //postal code
+    public void setPostal_code(int postal_code) {
+        this.postal_code = postal_code;
+    }
+    public int getPostal_code() {
+        return postal_code;
+    }
+    //////////////////////// end of setters and getters ///////////////////////////////////////////
 }
--- src/OnlineStore.java
+++ src/OnlineStore.java
@@ -1,32 +1,36 @@
+import org.jetbrains.annotations.NotNull;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class OnlineStore {
     static Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
         ArrayList<Customer> customers = new ArrayList<>();
 
-        System.out.println("********** Ali's ONLINE STORE **********" +
-                "\n" + "1) Customer" +
-                "\n" + "2) Client" +
-                "\n" + "3) Exit app " +
-                "\n" + "Please choose an option: ");
-        int x = input.nextInt();
-        switch (x) {
-            case 1:// Customer
+        System.out.println("********** Ali's ONLINE STORE **********");
+        int x;
+        do {
+            System.out.println("\n" + "1) Customer" +
+                    "\n" + "2) Client" +
+                    "\n" + "3) Exit app " +
+                    "\n" + "Please choose an option: ");
+            x = input.nextInt();
+            if (x == 1)// Customer
             {
                 System.out.println("1)Sign in" +
-                                 "\n2)Sign up" +
-                                 "\n3)Exit");
+                        "\n2)Sign up" +
+                        "\n3)Exit");
                 x = input.nextInt();
-                switch (x){
+                switch (x) {
                     case 1:
                         System.out.println();
                         break;
                     case 2:
                         Customer temp = new Customer();
                         customers.add(temp);
-
+                        customerSignUp(temp);
                         break;
                     case 3:
                         break;
@@ -34,15 +38,13 @@ public class OnlineStore {
                         System.out.println("please choose a valid option");
                 }
 
-            }
-                break;
-            case 2:// Client
-
+            } else if (x == 2)// Client
+            {
                 System.out.println("1)Sign in" +
                         "\n2)Sign up" +
                         "\n3)Exit");
                 x = input.nextInt();
-                switch (x){
+                switch (x) {
                     case 1:
                         break;
                     case 2:
@@ -52,16 +54,35 @@ public class OnlineStore {
                     default:
                         System.out.println("please choose a valid option");
                 }
-
+            } else if (x == 3) { //exit app
                 break;
-            case 3:
-                break;
-                default:
-                    System.out.println("please choose a valid option");
-        }
+            } else
+                System.out.println("please choose a valid option");
+        } while (x != 3);
 
     }
-    
+
+    static void customerSignUp(@NotNull Customer customer) {
+        System.out.println("please enter your specification to sign up:");
+        System.out.println("please enter a user name:");
+        customer.user_name = input.next();
+        System.out.println("please enter a password:");
+        customer.setPassword(input.next());
+//        System.out.println("please enter your name:");
+//        customer.setName(input.next());
+//        System.out.println("please enter your last_name:");
+//        customer.setLast_name(input.next());
+//        System.out.println("please enter your phone number:");
+//        customer.setPhone_number(input.nextInt());
+//        System.out.println("please enter your email:");
+//        customer.setEmail(input.next());
+//        System.out.println("please enter your address:");
+//        customer.setAddress(input.next());
+//        System.out.println("please enter your postal code:");
+//        customer.setPostal_code(input.nextInt());
+
+    }
+
 
 
 }
