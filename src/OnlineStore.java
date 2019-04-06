5cfd5b3fa74ca8261945086f6c9b7d7b179a7c90
sali72 <sahashemi072@gmail.com>
2019-04-06 16:57:49 +0430

    creating second screen of user interface

--- src/OnlineStore.java
+++ src/OnlineStore.java
@@ -3,15 +3,50 @@ import java.util.Scanner;
 public class OnlineStore {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("**********Ali's ONLINE STORE **********" +
-                    "\n" + "1) customer" +
-                    "\n" + "2) client" +
-                    "\n" + "3) exit app");
+        System.out.println("********** Ali's ONLINE STORE **********" +
+                "\n" + "1) Customer" +
+                "\n" + "2) Client" +
+                "\n" + "3) Exit app " +
+                "\n" + "Please choose an option: ");
         int x = input.nextInt();
         switch (x) {
             case 1:
+            {
+                System.out.println("1)Sign in" +
+                                 "\n2)Sign up" +
+                                 "\n3)Exit");
+                x = input.nextInt();
+                switch (x){
+                    case 1:
+                        System.out.println();
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    default:
+                        System.out.println("please choose a valid option");
+                }
+
+            }
                 break;
             case 2:
+
+                System.out.println("1)Sign in" +
+                        "\n2)Sign up" +
+                        "\n3)Exit");
+                x = input.nextInt();
+                switch (x){
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    default:
+                        System.out.println("please choose a valid option");
+                }
+
                 break;
             case 3:
                 break;
